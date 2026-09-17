fea2d1bb894b25b8ef02bf07cd24961407ab14d1
Agil Akbar Fahrezi <agilakbarfahrezi@mail.ugm.ac.id>
2026-09-17 12:07:53 +0700

    Foundation for planetary computer integration

--- src/ML_LC_Classifier/__init__.py
+++ src/ML_LC_Classifier/__init__.py
@@ -1,6 +1,7 @@
-"""Reusable land-cover training-data utilities."""
+"""Reusable pixel based classification pipeline for land cover mapping tasks"""
 
 from .classify_raster import classify_raster
+from .feature_stack import FeatureStackResult, create_feature_stack
 from .feature_elimination import FeatureSelectionResult, select_features
 from .load_extract import (
     class_distribution,
@@ -19,20 +20,34 @@ from .tune_model import (
     evaluate_model,
     tune_model,
 )
+from .planetary_computer import (
+    DownloadedAsset,
+    ImagerySearchResult,
+    download_imagery,
+    preview_imagery,
+    search_imagery,
+)
 
 __all__ = [
     "FeatureSelectionResult",
+    "FeatureStackResult",
+    "DownloadedAsset",
+    "ImagerySearchResult",
     "ModelEvaluation",
     "ModelTuningResult",
     "build_classifier",
     "class_distribution",
     "classify_raster",
+    "create_feature_stack",
+    "download_imagery",
     "drop_nan_samples",
     "extract_pixels_from_shapefile",
     "load_and_extract",
     "load_and_split_training_data",
     "load_training_samples",
     "open_raster",
+    "preview_imagery",
+    "search_imagery",
     "select_features",
     "train_test_split_data",
     "evaluate_model",
